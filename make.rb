97671e3efe6b402f238a81412b1f58d290e05960
Uran198 <uran198@gmail.com>
2014-09-12 22:38:51 +0300

    add example of post req & taking one problem

--- make.rb
+++ make.rb
@@ -6,8 +6,19 @@
 #   N is a number of contest
 #
 
+if ARGV.size < 1 || ARGV[0] == "--help"
+  puts <<STR
+Using:
+  make.rb cont [prob]
+  Where cont is number of contest,
+  prob is number of problem(if not specified takes all)
+  -- help - display this massage
+STR
+  exit
+end
+
 dir = ARGV[0]
-problems = 'A'..'E'
+problems = ARGV[1].nil? ? 'A'..'E' : [ARGV[1]]
 files = ["fetch_tests.rb", "run_tests.rb", "Makefile", "main.cpp"]
 cur = Dir.pwd
 files.map! { |x| x = cur+'/lib/'+x}
